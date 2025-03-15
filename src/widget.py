9cd2b48349b8c731d332fa787c33ab5669330b46
mikhail monakhov <mishobus@yandex.ru>
2025-03-16 00:10:03 +0300

    minor fixes

--- src/widget.py
+++ src/widget.py
@@ -1,7 +1,7 @@
-from src.masks import get_mask_card_number, get_mask_account
+from src.masks import get_mask_account, get_mask_card_number
 
 
-def mask_account_card(account_card: [str]) -> str:
+def mask_account_card(account_card: str) -> str:
     """
     Функция, которая определяет, получила ли она тип и номер карты или номер счёта, маскирует это и
     возвращает в нужном виде
@@ -29,9 +29,16 @@ def mask_account_card(account_card: [str]) -> str:
         return card_name + masked_card_number
 
 
-def get_date(data: [str]) -> str:
+def get_date(data: str) -> str:
+    """
+    Функция, которая вырезает из полученной строки определённые отрезки и затем проверяет наличие
+    лишних символов, после чего отправляет f-строкой дату в требуемом виде
+    """
+    # год
     year = data[:4]
+    # месяц
     month = data[5:7]
+    # день
     day = data[8:10]
     if year.isdigit() is not True or month.isdigit() is not True or day.isdigit() is not True:
         return "Ошибка: неверный формат данных"
@@ -39,15 +46,14 @@ def get_date(data: [str]) -> str:
         return f"{day}.{month}.{year}"
 
 
-
-#test_list = ["Maestro 1596837868705199",
-#"Счет 64686473678894779589",
-#"MasterCard 7158300734726758",
-#"Счет 35383033474447895560",
-#"Visa Classic 6831982476737658",
-#"Visa Platinum 8990922113665229",
-#"Visa Gold 5999414228426353",
-#"Счет 73654108430135874305"
-#]
-#for i in test_list:
+# test_list = ["Maestro 1596837868705199",
+# "Счет 64686473678894779589",
+# "MasterCard 7158300734726758",
+# "Счет 35383033474447895560",
+# "Visa Classic 6831982476737658",
+# "Visa Platinum 8990922113665229",
+# "Visa Gold 5999414228426353",
+# "Счет 73654108430135874305"
+# ]
+# for i in test_list:
 #    print(mask_account_card(i))
