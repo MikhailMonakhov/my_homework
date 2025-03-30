f63178a5b96b09012729c34854b0fecc3f345072
mikhail monakhov <mishobus@yandex.ru>
2025-03-30 11:58:06 +0300

    minor changes in feature

--- src/widget.py
+++ src/widget.py
@@ -44,16 +44,3 @@ def get_date(data: str) -> str:
         return "Ошибка: неверный формат данных"
     else:
         return f"{day}.{month}.{year}"
-
-
-# test_list = ["Maestro 1596837868705199",
-# "Счет 64686473678894779589",
-# "MasterCard 7158300734726758",
-# "Счет 35383033474447895560",
-# "Visa Classic 6831982476737658",
-# "Visa Platinum 8990922113665229",
-# "Visa Gold 5999414228426353",
-# "Счет 73654108430135874305"
-# ]
-# for i in test_list:
-#    print(mask_account_card(i))
